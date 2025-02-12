fcf4c9d65f5b1fc7cdc280d090f91c4ce0019a5c
rkerem <recep.kerem1d@gmail.com>
2025-02-12 05:12:18 +0300

    adding the "app" parameter to the socketio.run() 1.1

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ threading.Thread(target=start_async_loop, daemon=True).start()
 
 
 if __name__ == '__main__':
-    socketio.run(host="127.0.0.1", port=8000 ,debug=False)
+    socketio.run(app, host="127.0.0.1", port=8000 ,debug=False)
