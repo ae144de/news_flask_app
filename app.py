76729d7f12db471549b1cd9d5644b562a8b31448
rkerem <recep.kerem1d@gmail.com>
2025-02-12 04:29:33 +0300

    Update the localhost 1.1

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ threading.Thread(target=start_async_loop, daemon=True).start()
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8000 ,debug=False)
+    app.run(host="0.0.0.0", port=8000 ,debug=False)
